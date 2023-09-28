f9d953297ebced3ceb01cc11072f46e7ed5c348c
FranciscoJunior12 <105241871+FranciscoJunior12@users.noreply.github.com>
2023-09-29 00:27:09 +0200

    feat: created project structure and configured Express

--- App.js
+++ App.js
@@ -1,3 +1,21 @@
-const a = 20;
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
 
-console.log(a);
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;
---
+++ server.js
@@ -0,0 +1,7 @@
+import app from './App';
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`servidor rodando em http://localhost:${port}`);
+});
